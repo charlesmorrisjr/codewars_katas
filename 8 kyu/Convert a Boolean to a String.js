9ef05c146324d32d6e83e2f1f6dcc40d37e3d6c2
Charles Morris Jr <cmorrisjr777@gmail.com>
2025-10-05 16:34:35 -0400

    CW Kata 10-5-25

---
+++ 8 kyu/Convert a Boolean to a String.js
@@ -0,0 +1,34 @@
+/* Mini-PEDAC
+
+Problem:
+- Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+Input:
+- Boolean
+
+Output:
+- String
+
+Rules (Explicit/Implicit/Questions):
+- Input will always be a boolean value
+
+Data Structure:
+- Boolean
+- String
+
+Examples:
+true  => "true"
+false => "false"
+
+
+Algorithm:
+- Convert the input boolean to a string
+- Return the string value
+
+*/
+
+function booleanToString(b){
+  return String(b);
+}
