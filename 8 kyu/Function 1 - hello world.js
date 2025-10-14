2f170b50524e413c3046756956007b8792e75ee1
Charles Morris Jr <cmorrisjr777@gmail.com>
2025-10-13 23:24:58 -0400

    CW Kata 10-13-25

---
+++ 8 kyu/Function 1 - hello world.js
@@ -0,0 +1,28 @@
+/* Mini-PEDAC
+
+Problem:
+- Make a simple function called greet that returns the most-famous "hello world!".
+
+Input:
+- None
+
+Output:
+- String
+
+Rules (Explicit/Implicit/Questions):
+- Function should always return the string "hello world!"
+
+Data Structure:
+- String
+
+Examples:
+Calling greet() should return "hello world!"
+
+Algorithm:
+- Return "hello world!"
+
+*/
+
+function greet() {
+  return 'hello world!';
+}
