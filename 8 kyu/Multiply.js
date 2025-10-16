9c6734bc117d7263ac05ddfbb1c6d3dba37769d3
Charles Morris Jr <cmorrisjr777@gmail.com>
2025-10-15 23:49:58 -0400

    CW Kata 10-15-25

---
+++ 8 kyu/Multiply.js
@@ -0,0 +1,30 @@
+/* Mini-PEDAC
+
+Problem:
+- This code does not execute properly. Try to figure out why.
+
+Input:
+- Number
+- Number
+
+Output:
+- Number
+
+Rules (Explicit/Implicit/Questions):
+- Input number will always be a number
+- Input number will not be null/undefined
+
+Data Structure:
+- Number
+
+Examples:
+multiply(2,2), 4
+
+Algorithm:
+- Return the first input number multiplied by the second input number
+*/
+
+function multiply(a, b){
+  return a * b;
+}
+
