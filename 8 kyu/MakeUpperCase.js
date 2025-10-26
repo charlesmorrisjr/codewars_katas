2bf7b10400bf566c23c2da6fb40b21deea8485f3
Charles Morris Jr <cmorrisjr777@gmail.com>
2025-10-26 16:27:52 -0400

    CW Kata 10-26-25

---
+++ 8 kyu/MakeUpperCase.js
@@ -0,0 +1,31 @@
+/* Mini-PEDAC
+
+Problem:
+- Write a function which converts the input string to uppercase.
+
+Input:
+- String
+
+Output:
+- String
+
+Rules (Explicit/Implicit/Questions):
+- Input string will always be a string
+- Input string can be empty
+
+Data Structure:
+- String
+
+Examples:
+"hello" -> "HELLO"
+"" -> ""
+
+Algorithm:
+- Return the uppercase version of the input string
+
+*/
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
